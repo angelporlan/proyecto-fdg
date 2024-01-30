c99b23d12c748a5dd2b3c25559fb61765492269b
angelporlan <angeelporlan4@gmail.com>
2024-01-30 09:28:06 +0100

    tiempo solucionado

--- functions/functions.php
+++ functions/functions.php
@@ -21,21 +21,13 @@ function tiempoTranscurrido($fechaEnMilisegundos) {
 
     if ($diferencia->y > 0) {
         $formato .= 'Hace ' . $diferencia->y . ' año' . ($diferencia->y > 1 ? 's' : '') . ' ';
-    }
-
-    if ($diferencia->m > 0) {
+    } elseif ($diferencia->m > 0) {
         $formato .= 'Hace ' . $diferencia->m . ' mes' . ($diferencia->m > 1 ? 'es' : '') . ' ';
-    }
-
-    if ($diferencia->d > 0) {
+    } elseif ($diferencia->d > 0) {
         $formato .= 'Hace ' . $diferencia->d . ' día' . ($diferencia->d > 1 ? 's' : '') . ' ';
-    }
-
-    if ($diferencia->h > 0) {
+    } elseif ($diferencia->h > 0) {
         $formato .= 'Hace ' . $diferencia->h . ' hora' . ($diferencia->h > 1 ? 's' : '') . ' ';
-    }
-
-    if ($diferencia->i > 0) {
+    } else {
         $formato .= 'Hace ' . $diferencia->i . ' minuto' . ($diferencia->i > 1 ? 's' : '') . ' ';
     }
 
@@ -45,3 +37,4 @@ function tiempoTranscurrido($fechaEnMilisegundos) {
 
     return $formato;
 }
+
